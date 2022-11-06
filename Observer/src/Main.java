d5d778d90e432047c6be06960fa4d17ba2fa45d7
pwnzZzZz <99125058+pwnzZzZz@users.noreply.github.com>
2022-11-06 19:33:54 +0100

    Observer - Implementierung 1.0

---
+++ Observer/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Der Observer (Beobachter) ist ein Entwurfsmuster welches zur Kategorie Verhaltensmuster zählt.
+ * Dient zur Weitergabe von Änderungen an einem Objekt an von diesem Objekt abhängige Strukturen,
+ * wird verwendet wenn (viele) andere Objekte ein Update bei Änderung eines bestimmten Objekts (Subjekt) benötigen
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
