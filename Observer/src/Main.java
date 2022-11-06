e8da23f224cb96e3899cc5da5c1cf753e24e5e14
pwnzZzZz <99125058+pwnzZzZz@users.noreply.github.com>
2022-11-06 21:07:38 +0100

    Observer - Implementierung abgeschlossen

--- Observer/src/Main.java
+++ Observer/src/Main.java
@@ -7,5 +7,23 @@
 public class Main {
     public static void main(String[] args) {
 
+        StockGrabber stockGrabber = new StockGrabber();
+
+        StockObserver observer1 = new StockObserver(stockGrabber);
+        stockGrabber.setIBMPrice(197.00);
+        stockGrabber.setAAPLPrice(677.60);
+        stockGrabber.setGOOGPrice(676.40);
+
+        StockObserver observer2 = new StockObserver(stockGrabber);
+        stockGrabber.setIBMPrice(197.00);
+        stockGrabber.setAAPLPrice(677.60);
+        stockGrabber.setGOOGPrice(676.40);
+
+        stockGrabber.unregister(observer1);
+        stockGrabber.setIBMPrice(197.00);
+        stockGrabber.setAAPLPrice(677.60);
+        stockGrabber.setGOOGPrice(676.40);
+
+
     }
 }
